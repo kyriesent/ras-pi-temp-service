abb4de01733d64d5e41559fb1c93558bf33da7c1
David Jsa <david@jsa.me>
2018-05-15 03:58:04 +0000

    add periodic temp checking

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const fs = require('fs')
 const express = require('express')
+const cron = require('node-cron')
 
 const config = require('./config')
 
 const thermSerial = config.thermSerial
 
 console.log('Looking for thermometer with serial number ' + thermSerial + ' in /sys/bus/w1/devices/...')
-thermFile = fs.readFileSync('/sys/bus/w1/devices/' + thermSerial + '/w1_slave', { encoding: 'UTF-8' })
+thermFileName = '/sys/bus/w1/devices/' + thermSerial + '/w1_slave'
+thermFile = fs.readFileSync(thermFileName, { encoding: 'UTF-8' })
+
+var temp = 0;
+
+cron.schedule('* * * * * *', function () {
+	fs.readFile(thermFileName, { encoding: 'UTF-8' }, function (err, data) {
+		if (err) {
+			throw err
+		}
+
+		lines = data.split('\n')
+		statusString = lines[0].substr(lines[0].length - 3, lines[0].length)
+		if (statusString !== 'YES') { return }
+
+		tempString = lines[1].substr(lines[1].indexOf('t=') + 2)
+
+		temp = parseFloat(tempString)
+		temp = temp/1000
+	});
+})
 
 app = express()
 
